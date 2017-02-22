6bf85f55f536abd6e3898d2b4610c099d6e87df9
Ben Douglas <frulaa@yahoo.com>
2017-02-22 16:35:32 -0500

    Update NAO Com with examples

--- Python/Sensor-Com/nao_com.py
+++ Python/Sensor-Com/nao_com.py
@@ -1,14 +1,36 @@
-
-
 from naoqi import ALProxy
 import time
-tts = ALProxy("ALTextToSpeech", "192.168.1.137", 9559)
 
-motionProxy  = ALProxy("ALMotion", "192.168.1.137", 9559)
-postureProxy = ALProxy("ALRobotPosture", "192.168.1.137", 9559)
+nao_ip = "192.168.1.137"
+nao_port = "9559"
+
+# Create connection to NAO Text To Speech center
+tts = ALProxy("ALTextToSpeech", nao_ip, nao_port)
+
+# Create connection to NAO movement center
+motionProxy  = ALProxy("ALMotion", nao_ip, nao_port)
+
+# Create connection to NAO posture center (e.g., sit down, stand up)
+postureProxy = ALProxy("ALRobotPosture", nao_ip, nao_port)
+
+# Set TTS parameters
+tts.setParameter("speed", 75)
+tts.setParameter("pitchShift", 1.1)
 
 # Wake up robot
 motionProxy.wakeUp()
+
+# Make NAO speak
+tts.say("Hi, I'm Lucky. Lucky the robot.")
+time.sleep(2)
+tts.say("You've finished this round!")
+time.sleep(2)
+tts.say("You still managed to hit 2 times!")
+time.sleep(2)
+tts.say("You did not hit any last round.")
+time.sleep(2)
+
+exit()
 
 # Send robot to Stand Init
 postureProxy.goToPosture("Stand", .8)
@@ -18,8 +40,3 @@ time.sleep(2)
 # Go to rest position
 motionProxy.rest()
 
-# from time import gmtime, strftime
-#
-# for x in range(100):
-#     tts.say(strftime("Look at me, I'm an absurdly expensive clock! It's %I %M %p"))
-#     time.sleep(59)
